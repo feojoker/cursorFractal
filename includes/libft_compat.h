e18c26f1200a13610a7c8b255c31d6e71112123b
Fedor Sokolov <fedor.sokolov@dualbootpartners.com>
2025-09-21 11:03:32 +0400

    [#9.14] Missed file

--- includes/libft_compat.h
+++ includes/libft_compat.h
@@ -1,21 +1,19 @@
 #ifndef LIBFT_COMPAT_H
 #define LIBFT_COMPAT_H
 
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Simple implementations of needed libft functions
-static inline void ft_bzero(void *s, size_t n) {
-    memset(s, 0, n);
-}
+static inline void ft_bzero(void *s, size_t n) { memset(s, 0, n); }
 
 static inline char *ft_strncpy(char *dst, const char *src, size_t len) {
-    return strncpy(dst, src, len);
+  return strncpy(dst, src, len);
 }
 
 static inline void ft_itoa_simple(int num, char *str) {
-    sprintf(str, "%d", num);
+  sprintf(str, "%d", num);
 }
 
 // Alias for compatibility
