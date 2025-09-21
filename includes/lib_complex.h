c091babcecf9fc416351f139d72704006bf14552
Fedor Sokolov <fedor.sokolov@dualbootpartners.com>
2025-09-21 10:40:14 +0400

    [#9.5] Fix OpenCL header for Linux deployment
    - Add conditional compilation for macOS vs Linux
    - Provide minimal OpenCL type definitions for Linux
    - Keep full OpenCL support on macOS (local development)
    - Minimal change to fix Railway deployment

--- includes/lib_complex.h
+++ includes/lib_complex.h
@@ -1,49 +1,57 @@
 #ifndef _LIB_COMPLEX_H
-# define _LIB_COMPLEX_H
+#define _LIB_COMPLEX_H
 
-# include <OpenCL/opencl.h>
-# include "opencl-c-base.h"
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+// Linux OpenCL types (minimal definitions for compilation)
+typedef float float2 __attribute__((ext_vector_type(2)));
+typedef float float3 __attribute__((ext_vector_type(3)));
+typedef float float4 __attribute__((ext_vector_type(4)));
+typedef unsigned int uint;
+typedef unsigned int uint2 __attribute__((ext_vector_type(2)));
+#endif
 #include "math.h"
 
-# if TEST_DOUBLE_SUPPORT
-#  if defined(cl_khr_fp64)  // Khronos extension available?
-#   pragma OPENCL EXTENSION cl_khr_fp64 : enable
-#   define DOUBLE_SUPPORT_AVAILABLE
-#  elif defined(cl_amd_fp64)  // AMD extension available?
-#   pragma OPENCL EXTENSION cl_amd_fp64 : enable
-#   define DOUBLE_SUPPORT_AVAILABLE
-#  endif
-# endif
+#if TEST_DOUBLE_SUPPORT
+#if defined(cl_khr_fp64) // Khronos extension available?
+#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+#define DOUBLE_SUPPORT_AVAILABLE
+#elif defined(cl_amd_fp64) // AMD extension available?
+#pragma OPENCL EXTENSION cl_amd_fp64 : enable
+#define DOUBLE_SUPPORT_AVAILABLE
+#endif
+#endif
 
-# if defined(DOUBLE_SUPPORT_AVAILABLE)
- typedef double2		cl_complex;
- typedef cl_double4 	cl_quat;
- typedef double			TYPE;
-# else
- typedef float2			cl_complex;
- typedef float4			cl_quat;
- typedef float			TYPE;
-# endif
+#if defined(DOUBLE_SUPPORT_AVAILABLE)
+typedef double2 cl_complex;
+typedef cl_double4 cl_quat;
+typedef double TYPE;
+#else
+typedef float2 cl_complex;
+typedef float4 cl_quat;
+typedef float TYPE;
+#endif
 
-# define I_cl ((cl_complex)(0.0, 1.0))
-# define Pi_cl 3.14159265358979323846
-# define E_cl 2.718281828459045235360
+#define I_cl ((cl_complex)(0.0, 1.0))
+#define Pi_cl 3.14159265358979323846
+#define E_cl 2.718281828459045235360
 
-TYPE			cl_creal(cl_complex n);
-TYPE			cl_cimag(cl_complex n);
-TYPE			cl_cmod(cl_complex n);
-cl_complex		cl_cadd(cl_complex a, cl_complex b);
-cl_complex 		cl_cmult(cl_complex a, cl_complex b);
-cl_complex 		cl_cpow(cl_complex base , int exp);
-cl_complex 		cl_cdiv(cl_complex a, cl_complex b);
-TYPE 			cl_carg(cl_complex a);
-cl_complex 		cl_csqrt(cl_complex n);
-cl_complex 		cl_cexp(cl_complex n);
-cl_complex 		cl_clog(cl_complex z);
-TYPE			cl_cdot(cl_complex a, cl_complex b);
-cl_quat			cl_quat_mult(cl_quat q1, cl_quat q2);
-cl_quat 		cl_quat_sum(cl_quat q1, cl_quat q2);
-cl_quat 		cl_quat_conjugate(cl_quat q);
-TYPE 			cl_quat_mod(cl_quat q);
+TYPE cl_creal(cl_complex n);
+TYPE cl_cimag(cl_complex n);
+TYPE cl_cmod(cl_complex n);
+cl_complex cl_cadd(cl_complex a, cl_complex b);
+cl_complex cl_cmult(cl_complex a, cl_complex b);
+cl_complex cl_cpow(cl_complex base, int exp);
+cl_complex cl_cdiv(cl_complex a, cl_complex b);
+TYPE cl_carg(cl_complex a);
+cl_complex cl_csqrt(cl_complex n);
+cl_complex cl_cexp(cl_complex n);
+cl_complex cl_clog(cl_complex z);
+TYPE cl_cdot(cl_complex a, cl_complex b);
+cl_quat cl_quat_mult(cl_quat q1, cl_quat q2);
+cl_quat cl_quat_sum(cl_quat q1, cl_quat q2);
+cl_quat cl_quat_conjugate(cl_quat q);
+TYPE cl_quat_mod(cl_quat q);
 
 #endif
